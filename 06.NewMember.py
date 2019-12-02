9af7ae39f5ce7525e3c49773e56a47cf3ca6d880
Kasuczi <55882684+Kasuczi@users.noreply.github.com>
2019-12-02 09:52:44 +0100

    Update 06.NewMember.py

--- 06.NewMember.py
+++ 06.NewMember.py
@@ -1,4 +1,7 @@
 def openOrSenior(data):
+    """
+    the func returns the sum of the absolute value of each of the number's decimal digits
+    """
     lst = ''
     for i in data:
         if i[0] >= 55 and i[1] > 7:
