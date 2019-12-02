a20e6e04f60ad8f20687f14e1e1af01227a8a962
Kasuczi <55882684+Kasuczi@users.noreply.github.com>
2019-12-02 09:55:05 +0100

    Update 07.SumDigits.py

--- 07.SumDigits.py
+++ 07.SumDigits.py
@@ -1,3 +1,6 @@
 def sumDigits(number):
+    """
+    the func returns the sum of the absolute value of each of the number's decimal digits
+    """
     a = [int(i) for i in str(abs(number))]
     return sum(a)
