741fef72cca1af431fab78e8b4222d62663a2c36
Kasuczi <55882684+Kasuczi@users.noreply.github.com>
2019-12-02 09:46:05 +0100

    Update 01.JadenCase.py

--- 01.JadenCase.py
+++ 01.JadenCase.py
@@ -1,5 +1,8 @@
 import re
 def toJadenCase(string):
+    """
+    The func returns upper case for every word in the string
+    """
     return re.sub(r"[A-Za-z]+('[A-Za-z]+)?",
      lambda mo: mo.group(0)[0].upper() +
      mo.group(0)[1:].lower(),string)
