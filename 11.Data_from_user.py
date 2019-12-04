5b04fc774cbb28af32321a839f3ff2cc6f1b2e14
Kasuczi <55882684+Kasuczi@users.noreply.github.com>
2019-12-04 09:17:52 +0100

    Update 11.Data_from_user.py

--- 11.Data_from_user.py
+++ 11.Data_from_user.py
@@ -1,12 +1,14 @@
 class User:
 
-    def __init__(self, name, lastname, city): 
+    def __init__(self, name, lastname, city):
+        "the func initialize parameters which is going to be take from user"
         self._name = name
         self._lastname = lastname
         self._city = city
 
     @classmethod
     def from_input(cls):
+        "the func takes the data from user"
         return cls(
             input('First Name: '),
             input('Last Name: '), 
